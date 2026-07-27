ec4ab3c4aee0f36dcbebd92fcf1aedaf89ec67b0
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 22:09:48 +0600

    strings

--- strings.js
+++ strings.js
@@ -9,4 +9,29 @@ let loName = upName.toLowerCase().trim();
 
 console.log( loName.includes("hasan") );
 
-console.log( loName.indexOf("l") );
+console.log( loName.slice(0,5) );
+
+console.log( loName.slice(-2) );
+console.log( loName.replace("hasan", "shoumik"));
+
+
+//sting to array
+
+let fruits = "apple, komola, bedena, kola";
+
+let arr = fruits.split(",");
+console.log(arr);
+
+let joined= arr.join("  ");
+
+console.log(joined);
+
+//// concat////
+
+
+let firstname = "Taslimul";
+let lastname = "hasan";
+
+console.log( firstname.concat(" ", lastname , "||||| ").repeat(2));
+
+
