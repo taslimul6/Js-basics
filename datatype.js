641f7f5e420e9160d927fd9c73bc7760fc186e5f
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 18:11:53 +0600

    datatype

---
+++ datatype.js
@@ -0,0 +1,17 @@
+
+// sting data type
+
+let a ="i love england"
+
+//number
+
+let b = 1234
+
+//boolean
+
+let c = true
+
+console.log(!c);
+
+let d;
+console.log(d);
--- if-else.js
+++ if-else.js
@@ -1,6 +1,8 @@
+
 // if else with shorthand
 
 let age =18;
+
 
 if(age>= 18){
     console.log("you can vote");
@@ -9,7 +11,7 @@ if(age>= 18){
 
 let hasMoney= 100;
 
-if (hasMoney!==100){
+if (!hasMoney==100){
     console.log("buy a coffee");
 }else{
     console.log("drink water")
