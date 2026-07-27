427562919a7927ab88aabe433d85bfe3a6ab38d2
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 18:04:44 +0600

    if else

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// if else with shorthand
+
+let age =18;
+
+if(age>= 18){
+    console.log("you can vote");
+}
+
+
+let hasMoney= 100;
+
+if (hasMoney!==100){
+    console.log("buy a coffee");
+}else{
+    console.log("drink water")
+}
