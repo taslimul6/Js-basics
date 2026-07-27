b72ee6693f568560a9afa1af471564a0e79fa7dc
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 18:59:16 +0600

    module 7

---
+++ module7.js
@@ -0,0 +1,11 @@
+//identify data type
+
+let a ="abce";
+let b = 1234;
+let c = [5,55,6,45];
+let d = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(Array.isArray(c));
+console.log(typeof d);
