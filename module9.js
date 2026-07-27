776b19ef1cfd42a0314cbcb4ac32e3312ded4dc9
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 20:22:49 +0600

    loops

--- module9.js
+++ module9.js
@@ -21,3 +21,33 @@ do {
     console.log("while , colbe");
    
 };
+
+
+
+// loops in arrey
+
+const fruits= ["apple", "mango", "banana", "dragon fruit"];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
+    
+const numbers = [1,5,10,25,50];
+
+for(const number of numbers){
+  console.log(number);
+}
+
+//  loops in obsject
+
+let student={
+    "name" : "hasan",
+    "roll":"125"
+}
+
+console.log(typeof student);
+
+for(const key in student){
+    console.log(key ,": ", student[key]);
+
+}
