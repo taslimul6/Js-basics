267e12982733651956416f633408b88949e8c8e4
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 20:12:10 +0600

    loops

---
+++ module9.js
@@ -0,0 +1,23 @@
+
+
+for(let i =0; i<=4;i++ ){
+    console.log("hello");
+}
+
+
+
+let i= 0;
+
+while (i<=5) {
+    console.log("colse , colbe");
+    i++
+}
+
+i=1;
+do {
+    console.log("do , colbe");
+    i++
+} while (i<5){
+    console.log("while , colbe");
+   
+};
