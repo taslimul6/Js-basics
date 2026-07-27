7c5919e68eff73c59337466598a9180fac1c3a72
Taslimul Hasan <hasantaslimul@gmail.com>
2026-07-27 18:20:24 +0600

    addign and shorthand

--- datatype.js
+++ datatype.js
@@ -15,3 +15,40 @@ console.log(!c);
 
 let d;
 console.log(d);
+
+const pi = 3.1416;
+
+// modulas
+
+let x= 10;
+
+let y= x%2;
+
+console.log(y);
+
+//exponent
+
+y =x **2;
+
+console.log(y);
+
+// shorthands
+//increment
+let z =10;
+ z++;
+console.log("incremtent =>" , z);
+
+//decrement
+z =10;
+z--;
+
+console.log("decrement =>" , z);
+
+//  add and assign
+z =10;
+z+=10;
+console.log("added =>" , z);
+z =10;
+z*=10;
+
+console.log("multiply =>" , z);
