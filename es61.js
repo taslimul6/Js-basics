4e4dfcf39a4e00a4284f8f643900b10622f984d1
Taslimul Hasan <hasantaslimul@gmail.com>
2026-08-04 19:27:56 +0600

    es6 funda , scope, hoising

--- es61.js
+++ es61.js
@@ -1,23 +1,29 @@
 //scope, global, block , function
 
-//global
+//hoising
 
-var ami;
+
+//global
+console.log(ami2);
+// console.log(tmi);
+var ami2;
 let tmi;
 const hello="ami const";
 
-console.log(hello);
+// console.log(hello);
 
 if(true){
     var test ="ami if var";
     let test2 = "ami if let";
     const test3 = "ami if const";
 }
-console.log(test);
+// console.log(test);
 
 function ami(){
  var amina = "ami nai";
 }
-ami();
+
 
 // console.log(amina);
+
+
