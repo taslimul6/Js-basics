180f63a60f8a864fb44e6d5840fad5d8be4b1733
Taslimul Hasan <hasantaslimul@gmail.com>
2026-08-04 19:40:56 +0600

    template sting

---
+++ es6-templateString.js
@@ -0,0 +1,13 @@
+
+
+
+function greet(name =  "Guest", location = "badda", a,b){
+    return `Hello ${name}.
+    Welcome to the team.
+    we are located at ${location}.
+    your bill is ${a+b}
+    
+    `
+}
+
+console.log(greet(undefined,"dhaka",10,25));
