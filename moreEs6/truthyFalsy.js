bb5a1afae29aafe72c187428f0c2b17dbedf5ed5
Taslimul Hasan <hasantaslimul@gmail.com>
2026-08-07 10:02:15 +0600

    truthy falsy

---
+++ moreEs6/truthyFalsy.js
@@ -0,0 +1,17 @@
+//falsy are 6
+// false, 0 , "", null, undefined, NaN
+//baki sob truthy
+
+if("fa"){
+    console.log("truthy");
+
+}else {
+    console.log("Falsy");
+}
+
+if(undefined){
+    console.log("truthy");
+
+}else {
+    console.log("Falsy");
+}
