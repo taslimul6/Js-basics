5e1ed4430c7629c10d573556a571bee4aea70f59
Taslimul Hasan <hasantaslimul@gmail.com>
2026-08-07 15:21:35 +0600

    callback

---
+++ moreEs6/arrayPowerMethod.js
@@ -0,0 +1,7 @@
+
+
+//map
+
+let student = [10,20,30,40,50,];
+
+let st = student.map();
---
+++ moreEs6/callback.js
@@ -0,0 +1,60 @@
+
+
+
+function student(cb){
+    // console.log("ongoing registration process");
+
+    return  cb();
+}
+
+function basic(){
+    let std ={
+        name: "fotka",
+        rol:99
+    }
+    return std ;
+}
+
+function academic(){
+    let academia ={
+        "hsc" :4,
+        "ssc" :5
+    }
+    return academia;
+}
+
+
+// console.log(student(basic));
+
+// console.log(student(academic));
+
+
+// complex callback
+
+
+
+console.log(calculator( 10,20, displayResult))
+
+
+
+
+
+
+
+
+
+function calculator(a,b, cb){
+    let sum = a+b;
+    return cb(sum);
+}
+
+function displayResult(result){
+    return result;
+}
+
+
+
+
+
+
+
